af847b182d0eca85819a2c0c597a143f404ed58e
Saori Demizu <[sssuica@yahoo.co.jp]>
2019-04-19 17:49:05 +0900

    Edit image_uploader.rb

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,7 +1,7 @@
 class ImageUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  process resize_to_fit: [800, 800]
+  process resize_to_limit: [800, 800]
   include CarrierWave::MiniMagick
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -13,6 +13,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
